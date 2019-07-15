ed846bf1879370f40e5348afabbbfed9527112e0
xmcheng <1>
2019-07-15 14:40:14 +0800

    add

--- SpringDemo/src/main/java/com/study/spring/pattern/observer/SubjectDataBase.java
+++ SpringDemo/src/main/java/com/study/spring/pattern/observer/SubjectDataBase.java
@@ -4,11 +4,11 @@ public class SubjectDataBase extends EventListerner{
 
     public void selectSql(String sql){
         System.out.println("查询数据库");
-        printLog(sql);
+//        printLog(sql);
     }
 
     public void deleteSql(String sql){
         System.out.println("删除数据库");
-        printLog(sql);
+//        printLog(sql);
     }
 }
